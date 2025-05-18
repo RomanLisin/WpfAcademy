d696e7a76024a51381a755a675250a3a5caafccd
lisin <lisin@DESKTOP-P4T8JEF>
2025-05-18 18:06:51 +1000

    Remote DATABASE DONE

--- WpfAcademy/Models/DataProviders.cs
+++ WpfAcademy/Models/DataProviders.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Configuration;
 using CacheLibrary;
+using System.Windows;
 
 
 
@@ -19,8 +20,8 @@ namespace WpfAcademy.Models
 		public static DataSet LoadData()
 		{
 			Cache cache = new Cache(ConfigurationManager.ConnectionStrings["VPD_311_Import"].ConnectionString);
-
-			cache.AddTable("Students", "stud_id,last_name,first_name,[group]");
+			if (cache != null) { MessageBox.Show("✅ Подключение установлено"); } else { MessageBox.Show("Не удалось подключиться"); }
+				cache.AddTable("Students", "stud_id,last_name,first_name,[group]");
 			cache.AddTable("Groups", "group_id,group_name,direction");
 			cache.AddTable("Directions", "direction_id,direction_name");
 			cache.AddTable("Disciplines", "discipline_id,discipline_name,number_of_lessons");
